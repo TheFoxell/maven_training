06cc2ef95e0d2a747614d2730b37156434268b89
TheFoxell <44344136+TheFoxell@users.noreply.github.com>
2023-03-31 17:45:15 +0200

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,24 @@
+package fr.lernejo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @org.junit.jupiter.api.Test
+    void op() {
+        Sample sample = new Sample();
+        assertEquals(3, sample.op(Sample.Operation.ADD, 1, 2));
+        assertEquals(6, sample.op(Sample.Operation.MULT, 2, 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fact() {
+        Sample sample = new Sample();
+        assertEquals(1, sample.fact(0));
+        assertEquals(1, sample.fact(1));
+        assertEquals(2, sample.fact(2));
+        assertEquals(6, sample.fact(3));
+        assertEquals(24, sample.fact(4));
+        assertEquals(120, sample.fact(5));
+    }
+}
